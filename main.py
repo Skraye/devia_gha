6c9231e5c4332a321dcbbce130fcd17eb070cfa2
YannC <37600690+Skraye@users.noreply.github.com>
2025-02-06 12:21:47 +0100

    change in python file (#1)
    * change in python file
    
    * modify ci
    
    * modify script
    
    * modify ci
    
    * modify ci

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-
+# init app
 app = FastAPI()
 
 @app.get("/")
@@ -12,4 +12,12 @@ def return_double(number: int):
     return get_double(number)
 
 def get_double(number: int):
+    return number * 2
+
+
+@app.get("/2/{number}")
+def return_double2(number: int):
+    return get_double2(number)
+
+def get_double2(number: int):
     return number * 2
